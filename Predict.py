f157713b96711255675774460bc917651f1a8d2c
luobinguo64-bit <luobinguo64@gmail.com>
2026-03-16 19:51:00 +0800

    Update Predict.py

--- Predict.py
+++ Predict.py
@@ -62,6 +62,8 @@ if st.button("Make Prediction"):
     shap_values = explainer.shap_values(input_df)
 
     plt.figure(figsize=(10, 10))
+    plt.subplots_adjust(bottom=0.2) 
+    
     shap.force_plot(
         explainer.expected_value,
         shap_values[0],
