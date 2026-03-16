a29fd80b4805ccaf6db5422b6dece90c211bed57
luobinguo64-bit <luobinguo64@gmail.com>
2026-03-16 21:23:24 +0800

    Update Predict.py

--- Predict.py
+++ Predict.py
@@ -12,7 +12,7 @@ model = joblib.load('Catboost.pkl')  # 如果报错，推荐使用 CatBoost 的 
 # Streamlit UI
 st.markdown("<h3 style='font-size:28px'>Early Non-curative Recurrence Predictor</h3>", unsafe_allow_html=True)# 早期非_CURATIVE复发预测器
 
-st.markdown("<div style='margin-top:50px'></div>", unsafe_allow_html=True)
+st.markdown("<div style='margin-top:40px'></div>", unsafe_allow_html=True)
 
 # Sidebar for input options
 AFP = st.sidebar.number_input("Alpha-fetoprotein (AFP, ng/mL):", min_value=0.0, max_value=100000.0, value=20.0)  
@@ -41,7 +41,7 @@ if st.button("Make Prediction"):
     probability = predicted_proba[1] * 100
 
     # Display single line
-    st.markdown("<div style='margin-top:50px'></div>", unsafe_allow_html=True)
+    st.markdown("<div style='margin-top:40px'></div>", unsafe_allow_html=True)
     
     st.markdown(
         f"<p style='font-family:Source Serif; font-size:18px; font-weight:bold;'>Predicted possibility of early non-curative recurrence is {probability:.1f}%</p>",
@@ -59,14 +59,14 @@ if st.button("Make Prediction"):
             unsafe_allow_html=True
         )
         
-    st.markdown("<div style='margin-top:50px'></div>", unsafe_allow_html=True)
+    st.markdown("<div style='margin-top:40px'></div>", unsafe_allow_html=True)
 
     # SHAP 解释
     explainer = shap.TreeExplainer(model)
     shap_values = explainer.shap_values(input_df)
 
     # 绘制 force_plot
-    plt.figure(figsize=(14, 30), dpi=150)  # 增大竖向高度
+    plt.figure(figsize=(14, 30), dpi=1000)  # 增大竖向高度
     plt.rcParams.update({'font.size': 20})  # 字体大一些
     shap.force_plot(
     explainer.expected_value,
