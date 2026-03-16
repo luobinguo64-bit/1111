de647a9edfe7b823ff1e44394b647eb764d53f7e
luobinguo64-bit <luobinguo64@gmail.com>
2026-03-16 19:32:46 +0800

    Update Predict.py

--- Predict.py
+++ Predict.py
@@ -60,9 +60,9 @@ else:
 # 设置全局字体和大小
 plt.rcParams["font.family"] = "Source Serif"
 plt.rcParams["axes.unicode_minus"] = False
-plt.rcParams["font.size"] = 14
-plt.rcParams["xtick.labelsize"] = 12
-plt.rcParams["ytick.labelsize"] = 12
+plt.rcParams["font.size"] = 20
+plt.rcParams["xtick.labelsize"] = 18
+plt.rcParams["ytick.labelsize"] = 18
 
 # SHAP 解释
 explainer = shap.TreeExplainer(model)
