adf73b69342b65ffb3d4e71925832498de6524ba
luobinguo64-bit <luobinguo64@gmail.com>
2026-03-16 19:44:52 +0800

    Update Predict.py

--- Predict.py
+++ Predict.py
@@ -61,7 +61,7 @@ if st.button("Make Prediction"):
     explainer = shap.TreeExplainer(model)
     shap_values = explainer.shap_values(input_df)
 
-    plt.figure(figsize=(15, 8))
+    plt.figure(figsize=(15, 12))
     shap.force_plot(
         explainer.expected_value,
         shap_values[0],
